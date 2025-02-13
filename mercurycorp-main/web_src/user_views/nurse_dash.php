c1f321c8b9fd92bc1f118ce327ef7457cd535707
jxmill <jxm703@ptd.net>
2025-02-12 21:27:31 -0500

    Testing Chatbox in nurse_dash.php

--- mercurycorp-main/web_src/user_views/nurse_dash.php
+++ mercurycorp-main/web_src/user_views/nurse_dash.php
@@ -377,8 +377,8 @@ try {
   <img class="socialMediaIcon" src="../photos/twitter.png" alt="Twitter">
 </a>
 </footer>
-<div id= "chat-container" style= "position: fixed; bottom: 20px;  right: 20px; width: 300px; height: 400px; background: white; border-radius: 8px;">
-    <div id="chat-box" style="height: 80%; overflow-y: scroll; background: white; border-radius: 5px; margin-bottom: 10px;">
+<div id= "chat-container" style= "position: fixed; bottom: 20px;  right: 20px; width: 300px; height: 400px; background: black; border-radius: 8px;">
+    <div id="chat-box" style="height: 80%; overflow-y: scroll; background: black; border-radius: 5px; margin-bottom: 10px;">
 
         </div>
         <input type="text" id="user-message" placeholder="Type a message..." style="width: 80%; padding: 8px; border-radius: 5px;">
